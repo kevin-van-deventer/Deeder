1e90820af7c7190520e7eb8e0fb69c7c3e4591f7
kevin-van-deventer <kevinlenovomail@gmail.com>
2025-02-20 06:44:16 +0200

    maps counter working again

--- backend/app/channels/application_cable.rb
+++ backend/app/channels/application_cable.rb
@@ -1,32 +1,29 @@
 module ApplicationCable
-    class Connection < ActionCable::Connection::Base
-      identified_by :current_user
-  
-      def connect
-        self.current_user = find_verified_user
-      end
-  
-      private
-  
-      def find_verified_user
-        token = request.params[:token]
-  
-        if token.present?
-          begin
-            decoded_token = JWT.decode(token, Rails.application.secrets.secret_key_base, true, algorithm: 'HS256')
-            user_id = decoded_token[0]["user_id"]
-            if verified_user = User.find_by(id: user_id)
-              return verified_user
-            end
-  
-            # return user if user.present?
-          rescue JWT::DecodeError
-            reject_unauthorized_connection
-          end
+  class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+      logger.add_tags "ActionCable", current_user.id
+    end
+
+    private
+
+    def find_verified_user
+      token = request.params[:token]
+
+      if token.present?
+        begin
+          decoded_token = JWT.decode(token, Rails.application.secret_key_base, true, algorithm: 'HS256')[0]
+          user = User.find_by(id: decoded_token["user_id"])
+
+          return user if user.present?
+        rescue JWT::DecodeError
+          Rails.logger.warn "Invalid JWT Token: Connection rejected"
         end
-  
-        reject_unauthorized_connection
       end
+
+      reject_unauthorized_connection
     end
   end
-  
+end
