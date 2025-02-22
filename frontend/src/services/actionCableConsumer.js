b017bd8f897497f2986a1dc486c5d937bf9be984
kevin-van-deventer <kevinlenovomail@gmail.com>
2025-02-22 13:18:11 +0200

    fixed api routes for frontend

--- frontend/src/services/actionCableConsumer.js
+++ frontend/src/services/actionCableConsumer.js
@@ -1,6 +1,7 @@
 import { createConsumer } from "@rails/actioncable";
 
 const token = localStorage.getItem("token");
-const consumer = createConsumer(`ws://localhost:3000/cable?token=${token}`);
+// const consumer = createConsumer(`ws://localhost:3000/cable?token=${token}`);
+const consumer = createConsumer(`ws://young-dusk-69972-fa429cadde5b.herokuapp.com/?token=${token}`);
 
 export default consumer;
