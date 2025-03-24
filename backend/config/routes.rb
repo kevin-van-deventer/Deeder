288620bb1fbfd660227823c0271348cd5ece2f6c
kevin-van-deventer <kevinlenovomail@gmail.com>
2025-03-24 12:17:33 +0200

    static controller added

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -30,4 +30,6 @@ Rails.application.routes.draw do
   # Authentication
   post '/login', to: 'users#login'
 
+  get '*path', to: 'static#index', constraints: ->(req) { !req.xhr? && req.format.html? }
+
 end
