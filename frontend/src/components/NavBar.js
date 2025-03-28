0616bfbb58823a6f338dfa9400c567cd064528a6
kevin-van-deventer <kevinlenovomail@gmail.com>
2025-03-28 12:20:54 +0200

    navbar logout reload removed

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,54 +1,62 @@
-import { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import "./NavBar.css"; // Import CSS for styling
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import "./NavBar.css" // Import CSS for styling
 
 const Navbar = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem("token"));
-  const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem("token"))
+  const navigate = useNavigate()
 
   useEffect(() => {
     const handleStorageChange = () => {
-      setIsLoggedIn(!!localStorage.getItem("token"));
-    };
+      setIsLoggedIn(!!localStorage.getItem("token"))
+    }
 
-    window.addEventListener("storage", handleStorageChange);
+    window.addEventListener("storage", handleStorageChange)
     return () => {
-      window.removeEventListener("storage", handleStorageChange);
-    };
-  }, []);
+      window.removeEventListener("storage", handleStorageChange)
+    }
+  }, [])
 
   const handleLogout = () => {
-    localStorage.removeItem("token");
-    setIsLoggedIn(false);
-    navigate("/login");
-    window.location.reload(); // Ensures AppRouter updates
-  };
+    localStorage.removeItem("token")
+    setIsLoggedIn(false)
+    navigate("/")
+    // window.location.reload() // Ensures AppRouter updates
+  }
 
   return (
     <nav className="navbar">
       <ul className="nav-list">
-        <h2 className="webTitle">
-          Deeder
-        </h2>
+        <h2 className="webTitle">Deeder</h2>
         <li>
-          <Link to="/dashboard" className="nav-link">Dashboard</Link>
+          <Link to="/dashboard" className="nav-link">
+            Dashboard
+          </Link>
         </li>
         <li>
-          <Link to="/maps" className="nav-link">Map</Link>
+          <Link to="/maps" className="nav-link">
+            Map
+          </Link>
         </li>
         <li>
-          <Link to="/chat" className="nav-link">Chat</Link>
+          <Link to="/chat" className="nav-link">
+            Chat
+          </Link>
         </li>
         <li>
           {isLoggedIn ? (
-            <button onClick={handleLogout} className="nav-button">Logout</button>
+            <button onClick={handleLogout} className="nav-button">
+              Logout
+            </button>
           ) : (
-            <Link to="/login" className="nav-link">Login</Link>
+            <Link to="/login" className="nav-link">
+              Login
+            </Link>
           )}
         </li>
       </ul>
     </nav>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
