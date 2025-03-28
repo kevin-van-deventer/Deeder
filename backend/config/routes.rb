fc13ede2118e7e5ef2048ea4b193b81658276599
kevin-van-deventer <kevinlenovomail@gmail.com>
2025-03-28 12:57:40 +0200

    routes added signup

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -30,7 +30,7 @@ Rails.application.routes.draw do
 
   # Authentication
   post '/login', to: 'users#login'
-  get '/signup', to: 'static#index'
+  get '/signup', to: 'users#signup'
   # post '/signup', to: 'users#signup'
 
   get '*path', to: 'static#index', constraints: ->(req) { !req.xhr? && req.format.html? }
