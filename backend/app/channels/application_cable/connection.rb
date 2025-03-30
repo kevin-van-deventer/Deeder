1b6600cb26ffdf022ae9315be9c2833b124e9477
kevin-van-deventer <kevinlenovomail@gmail.com>
2025-03-30 13:40:40 +0200

    updated connection.rb

--- backend/app/channels/application_cable/connection.rb
+++ backend/app/channels/application_cable/connection.rb
@@ -1,27 +1,30 @@
 module ApplicationCable
-    class Connection < ActionCable::Connection::Base
-      identified_by :current_user
-  
-      def connect
-        self.current_user = find_verified_user
-        logger.add_tags "ActionCable", "User #{current_user.id}" # ✅ Log successful connection
-        logger.info "[ActionCable] WebSocket connection established for user #{current_user.id}"
-      end
-  
-      private
-  
-      def find_verified_user
-        token = request.params[:token]
-        if (token)
+  class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+      logger.add_tags "ActionCable", current_user.id
+    end
+
+    private
+
+    def find_verified_user
+      token = request.params[:token]
+
+      if token.present?
+        begin
           decoded_token = JWT.decode(token, Rails.application.credentials.secret_key_base, true, { algorithm: 'HS256' })[0]
-          user_id = decoded_token["user_id"]
-          verified_user = User.find_by(id: user_id)
-          return verified_user if verified_user
+          user = User.find_by(id: decoded_token["user_id"]) || reject_unauthorized_connection
+
+          return user if user.present?
+        rescue JWT::ExpiredSignature
+          reject_unauthorized_connection
+        rescue JWT::DecodeError
+          reject_unauthorized_connection
         end
-        reject_unauthorized_connection
-      rescue JWT::ExpiredSignature, JWT::DecodeError
-        reject_unauthorized_connection
       end
+      reject_unauthorized_connection
+    end
   end
 end
-  
